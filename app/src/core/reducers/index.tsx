2d0bc7e18dae1f222aeb43ff4fcfee17abddc0b9
Arthur Deshays <a.deshays@gmail.com>
2022-05-17 19:57:07 +0200

    MOD : reducer

--- app/src/core/reducers/index.tsx
+++ app/src/core/reducers/index.tsx
@@ -1,11 +1,31 @@
 import { combineReducers } from "redux"
+import { itemsDB } from "../../Models/items"
 import { Action, Item, ItemState } from "../../types"
+import itemsActions from "./actions"
 
 
+const InitialState : ItemState = {
+    loading : false,
+    success : false,
+    items : itemsDB
+}
 type Payloads = Item | Item[]
 const ItemReducer = (state : ItemState, action:Action<Payloads>) => {
 
+    switch (action.type){
+        case "ADD_ITEM":
+            return {
+                ...state,
+                items : [...itemsDB, action.payload as Item]
+            }
+        case "DELETE_ITEM":
+            return {
+                ...state,
+                items : state.items.filter((item) => item.id === (action.payload as Item).id)
+            }
+    }
 }
+
 const IndexReducers = () => combineReducers({})
 
 export default IndexReducers
