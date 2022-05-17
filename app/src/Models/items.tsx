e0d90748d4e69b21a4d12fe55972d6001ba64ff5
Arthur Deshays <a.deshays@gmail.com>
2022-05-17 19:40:17 +0200

    ADD: models items

---
+++ app/src/Models/items.tsx
@@ -0,0 +1,16 @@
+import { Item } from "../types";
+
+export const Items : Item[] = [
+    {
+        id: "",
+        title: "",
+        description: "",
+        imgUrl: "" 
+    },
+    {
+        id:"",
+        title: "",
+        description: "",
+        imgUrl: "" 
+    }
+]
